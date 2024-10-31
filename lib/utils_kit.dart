4d9b00f27041c9a353cf2003d8c5a249a3fa678a
martin.pettersen <martin.pyk.pettersen@lyse.no>
2024-10-31 10:03:08 +0100

    Add build context extensions

--- lib/utils_kit.dart
+++ lib/utils_kit.dart
@@ -2,6 +2,7 @@ export 'package:utils_kit/src/async/task_queue.dart';
 export 'package:utils_kit/src/components/lifecycle_listener.dart';
 export 'package:utils_kit/src/components/spaced_column.dart';
 export 'package:utils_kit/src/components/spaced_row.dart';
+export 'package:utils_kit/src/extensions/build_context_extensions.dart';
 export 'package:utils_kit/src/extensions/list_extensions.dart';
 export 'package:utils_kit/src/extensions/orientation_extensions.dart';
 export 'package:utils_kit/src/platform/platform_utils.dart';
